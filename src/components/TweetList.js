f2510ae4dfffa054ea5f704d23dd89cd7f0e397e
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-10 10:45:36 +0700

    :sparkles: Can render tweet list from app state.

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
+const Tweet = styled.p`
+  border: 1px solid #000;
+`
+const Container = styled.div`
+  max-width: 100%;
+`
 class TweetList extends Component {
+  renderTweetList = () =>
+    this.props.tweetList.map(tweet => <Tweet key={tweet}>{tweet}</Tweet>)
   render() {
+    const { tweetList } = this.props
     return (
-      <div>
-        <p>tweet 1</p>
-        <p>tweet 1</p>
-        <p>tweet 1</p>
-        <p>tweet 1</p>
-        <p>tweet 1</p>
-        <p>tweet 1</p>
-        <p>tweet 1</p>
-      </div>
+      <Container>
+        {tweetList.length > 0 ? this.renderTweetList() : <span>no tweet</span>}
+      </Container>
     )
   }
 }

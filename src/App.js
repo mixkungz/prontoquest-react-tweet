8306fc90aac5563d8769fbc786c7470cb0cdfa09
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-10 10:00:12 +0700

    :construction: Add tweet form & list component show on app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// React & Library
+import React, { Component } from 'react'
+
+// Local
+import './App.css'
+import logo from './logo.svg'
+import { TweetForm } from './components/TweetForm'
+import { TweetList } from './components/TweetList'
 
 class App extends Component {
   render() {
@@ -8,21 +13,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="container">
+          <TweetForm />
+          <TweetList />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

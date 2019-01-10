b24c8db1cb44f098fe0d4d530923f4f4003484fd
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-10 10:41:16 +0700

    :sparkles: Create tweetList state, add tweet func and send it to child as a props.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ import { TweetForm } from './components/TweetForm'
 import { TweetList } from './components/TweetList'
 
 class App extends Component {
+  state = {
+    tweetList: []
+  }
+
+  addTweet = async newTweet => {
+    await this.setState(prevState => {
+      prevState.tweetList.unshift(newTweet)
+      return {
+        tweetList: prevState.tweetList
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,8 +28,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <div className="container">
-          <TweetForm />
-          <TweetList />
+          <TweetForm addTweet={this.addTweet} />
+          <TweetList tweetList={this.state.tweetList} />
         </div>
       </div>
     )
